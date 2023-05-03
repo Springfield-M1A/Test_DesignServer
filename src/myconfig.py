2c44827d159dda1698dce39536e5b47ae9e9f67e
M1A_ <120645103+Springfield-M1A@users.noreply.github.com>
2023-05-03 12:37:02 +0900

    Update myconfig.py

--- src/myconfig.py
+++ src/myconfig.py
@@ -4,16 +4,16 @@ import os
 def get_config(filepath):
     """Get config parser.
 
-        :param filepath: a config filepath to read.
-            :type filepath: str
-                :return: a parser (Use it like a "dictionary")
-                    :rtype: configparser.SafeConfigParser
-                        """
-                            parser = SafeConfigParser(os.environ)
-                                ret = parser.read(filepath)
-                                    if not ret:
-                                                print('get_config(): Failed to parser.read()')
-                                                        return False
-                                                            return parser
+    :param filepath: a config filepath to read.
+    :type filepath: str
+    :return: a parser (Use it like a "dictionary")
+    :rtype: configparser.SafeConfigParser
+    """
+    parser = SafeConfigParser(os.environ)
+    ret = parser.read(filepath)
+    if not ret:
+        print('get_config(): Failed to parser.read()')
+        return False
+    return parser
 
 
